bdf3fb641897d0a9308d9c5d1db85f1c79eb9593
Pedro Eugenio Antunes <peantunes@gmail.com>
2019-12-13 14:58:57 +0000

    Initial Commit

---
+++ ClearSale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearSale
+//
+//  Created by Pedro Antunes on 13/12/2019.
+//  Copyright © 2019 Pedro Antunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
